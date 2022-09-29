bb1f0be6a2979564125858493f48036e673b4532
muhamad satria <114433514+mhmdsatria@users.noreply.github.com>
2022-09-29 11:15:36 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,18 @@
+# aplikasi biodata create by muhamad satria
+
+# memasukan nama
+nama= input("masukin nama anda!")\
+# output nama
+print("nama anda adalah; ",nama)
+# input kelas
+kelas=input("masukin kelas anda!")\
+#output kelas
+print("kelas anda adalah; ",kelas)
+#input NIM
+NIM= input("masukin NIM anda!")\
+#output
+print("NIM anda adalah; ",NIM)
+#input prodi
+prodi=input("masukin prodi anda!")\
+#output prodi
+print("prodi anda adalah; ",prodi)
