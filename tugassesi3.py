ae287dac7d03c8112fc4c8df3ac4ae4d0d14d85f
muhamad satria <114433514+mhmdsatria@users.noreply.github.com>
2022-10-06 09:22:22 +0700

    Add files via upload

---
+++ tugassesi3.py
@@ -0,0 +1,13 @@
+#tugas sesi 3
+#print
+print("lengkapi datamu sekarang juga...")
+#variable data
+nama = "Muhamad Satria"
+kelas = "TI22A"
+jurusan = "Teknik Informatika"
+quote = "Hiduplah untuk mati besok"
+#print
+print("nama      :", nama)
+print("kelas     :", kelas)
+print("jurusan   :", jurusan)
+print("quote     :", quote)
